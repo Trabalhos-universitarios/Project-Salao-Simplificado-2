d91648abd6d344263d6f89d1a39934f37acabdae
Bruna Boccaldi <boccaldibruna@gmail.com>
2022-11-27 12:15:54 -0300

    commit

--- front-end/src/admin/components/home-admin/home-admin.component.ts
+++ front-end/src/admin/components/home-admin/home-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-admin',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeAdminComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  public addFunc() {
+    this.router.navigate(['admin/register-new-employee'])
   }
 
 }
